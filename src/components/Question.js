b8c48e90608e6a64a38ad2044ae1ef058a9441c7
Romulo Costa <rgcostabox@gmail.com>
2020-10-06 15:59:56 -0400

    Add bootstrap

---
+++ src/components/Question.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Question extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    This is some text within a card body.
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Question;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import { createStore } from "redux";
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 import middlewares from './middlewares';
+import Question from './components/Question'
 
 const store = createStore(reducers, middlewares);
 
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
-          <App />
+          <Question />
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
